6b747f5dadeec1e94be53ff8431ed194ec89fbfc
Md. Yeasin Shovan <shovan642@gmail.com>
2023-02-13 21:46:11 +0600

    first commit

---
+++ login.js
@@ -0,0 +1,15 @@
+document.getElementById('submit-button').addEventListener('click',function(){
+    const getEmailAddressField = document.getElementById('email-address');
+    const getEmailAddressValue = getEmailAddressField.value;
+
+    const getPasswordField = document.getElementById('password');
+    const getPasswordValue = getPasswordField.value;
+
+    if(getEmailAddressValue==="google@gmail.com" && getPasswordValue==="mygoogle"){
+        window.location.href="myaccount.html";
+    }
+    else{
+        console.log('invalid user')
+    }
+
+})
